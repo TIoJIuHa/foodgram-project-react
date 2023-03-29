1b393dca7c22312b28175a51d693e1533694fe47
TIoJIuHa <p.elistratova03@mail.ru>
2023-03-29 20:45:57 +0300

    added management command for import ingredients

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,9 @@ from .views import RecipeViewSet, TagViewSet, IngredientViewSet
 
 router = DefaultRouter()
 
-router.register("recipes", RecipeViewSet, basename="recipes")
-router.register("tags", TagViewSet, basename="tags")
-router.register("ingredients", IngredientViewSet, basename="ingredients")
+router.register(r"recipes", RecipeViewSet, basename="recipes")
+router.register(r"tags", TagViewSet, basename="tags")
+router.register(r"ingredients", IngredientViewSet, basename="ingredients")
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,22 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+from recipes.models import Ingredient
+
+TABLES = {
+    Ingredient: "ingredients.csv"
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        filename = f"{settings.BASE_DIR}/../data/ingredients.csv"
+        with open(filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f, fieldnames=('name', 'measurement_unit'))
+            for data in reader:
+                ingredient = Ingredient(
+                    name=data["name"],
+                    measurement_unit=data["measurement_unit"]
+                )
+                ingredient.save()
